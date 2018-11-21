ea49d79947e2f567414037c067300c9e45aea45f
x1000z500 <lbarrosov@gmail.com>
2018-11-21 20:47:48 +0100

    Day 2.

--- app/src/main/java/l/b/v/udacitytestapp/lesson2/AboutMeActivity.kt
+++ app/src/main/java/l/b/v/udacitytestapp/lesson2/AboutMeActivity.kt
@@ -1,7 +1,13 @@
 package l.b.v.udacitytestapp.lesson2
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.about_me_layout.*
 import l.b.v.udacitytestapp.R
 
 class AboutMeActivity: AppCompatActivity() {
@@ -10,6 +16,21 @@ class AboutMeActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.about_me_layout)
+
+        buttonDone.setOnClickListener { addNickName(it) }
+    }
+
+    fun addNickName(view: View){
+
+        editText.visibility = GONE
+        view.visibility = GONE
+
+        textView2.text = editText.text
+        textView2.visibility = VISIBLE
+
+        val inm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 
 }
