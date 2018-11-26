de8779fb26b4e35798fe05d317b318cbdd2e33eb
x1000z500 <lbarrosov@gmail.com>
2018-11-26 23:19:08 +0100

    Added DataBinding.

--- app/src/main/java/l/b/v/udacitytestapp/lesson2/AboutMeActivity.kt
+++ app/src/main/java/l/b/v/udacitytestapp/lesson2/AboutMeActivity.kt
@@ -1,32 +1,42 @@
 package l.b.v.udacitytestapp.lesson2
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.inputmethod.InputMethodManager
-import kotlinx.android.synthetic.main.about_me_layout.*
 import l.b.v.udacitytestapp.R
+import l.b.v.udacitytestapp.databinding.AboutMeLayoutBinding
+import l.b.v.udacitytestapp.lesson2.databinding.MyName
 
 class AboutMeActivity: AppCompatActivity() {
+
+    private lateinit var binding: AboutMeLayoutBinding
+    private var myNameVar = MyName("Luis Barroso", "luisNickname")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //Delete to use databinding
         setContentView(R.layout.about_me_layout)
 
-        buttonDone.setOnClickListener { addNickName(it) }
+        binding = DataBindingUtil.setContentView(this,R.layout.about_me_layout)
+        binding.myName = myNameVar
+        binding.buttonDone.setOnClickListener { addNickName(it) }
     }
 
     fun addNickName(view: View){
 
-        editText.visibility = GONE
         view.visibility = GONE
-
-        textView2.text = editText.text
-        textView2.visibility = VISIBLE
+        binding.apply {
+            editText.visibility = GONE
+            myName?.nickName = editText.text.toString()
+            invalidateAll()
+            binding.textView2.visibility = VISIBLE
+        }
 
         val inm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inm.hideSoftInputFromWindow(view.windowToken, 0)
---
+++ app/src/main/java/l/b/v/udacitytestapp/lesson2/databinding/MyName.kt
@@ -0,0 +1,3 @@
+package l.b.v.udacitytestapp.lesson2.databinding
+
+data class MyName(var name: String, var nickName: String)
