98b977e0683d63236e5d6f2670692dac4fa29f98
Luis Barroso Vazquez <luis.barroso.vazquez@accenture.com>
2018-11-28 09:15:49 +0100

    Lesson 2 finished.

--- app/src/main/java/l/b/v/udacitytestapp/lesson2/part2/ColorMyViewsActivity.kt
+++ app/src/main/java/l/b/v/udacitytestapp/lesson2/part2/ColorMyViewsActivity.kt
@@ -12,7 +12,8 @@ class ColorMyViewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color_my_views)
 
-        val listBoxes = listOf<View>(textView1, textView2,textView3,textView4,textView5,constaintLayoutBackground)
+        val listBoxes = listOf<View>(textView1, textView2,textView3,textView4,textView5,constaintLayoutBackground,
+                button_red,button_yellow,button_green)
 
         listBoxes.forEach {
             it.setOnClickListener {
@@ -22,7 +23,12 @@ class ColorMyViewsActivity : AppCompatActivity() {
                     R.id.textView3 -> it.setBackgroundColor(getColor(android.R.color.holo_orange_dark))
                     R.id.textView4 -> it.setBackgroundColor(getColor(android.R.color.holo_green_dark))
                     R.id.textView5 -> it.setBackgroundColor(getColor(android.R.color.holo_purple))
-                    else -> it.setBackgroundColor(getColor(android.R.color.background_dark))
+
+                    R.id.button_red -> textView3.setBackgroundResource(R.color.color_red)
+                    R.id.button_yellow -> textView4.setBackgroundResource(R.color.color_yellow)
+                    R.id.button_green -> textView5.setBackgroundResource(R.color.color_green)
+
+                    else -> it.setBackgroundColor(getColor(R.color.color_grey))
                 }
             }
         }
