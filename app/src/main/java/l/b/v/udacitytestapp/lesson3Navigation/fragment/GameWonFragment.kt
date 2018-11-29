95dc551b5ade95e98542c777281bd9969337ad85
Luis Barroso Vazquez <luis.barroso.vazquez@accenture.com>
2018-11-29 17:51:22 +0100

    Added option menu in win fragment to share the score.

--- app/src/main/java/l/b/v/udacitytestapp/lesson3Navigation/fragment/GameWonFragment.kt
+++ app/src/main/java/l/b/v/udacitytestapp/lesson3Navigation/fragment/GameWonFragment.kt
@@ -4,9 +4,7 @@ package l.b.v.udacitytestapp.lesson3Navigation.fragment
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.navigation.findNavController
 import l.b.v.udacitytestapp.R
@@ -26,7 +24,14 @@ class GameWonFragment : Fragment() {
         var args = GameWonFragmentArgs.fromBundle(arguments)
         Toast.makeText(context, "Total questions: ${args.numQuestions}, Questions correct: ${args.numCorrect}",Toast.LENGTH_LONG).show()
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.winner_menu, menu)
     }
 
 
