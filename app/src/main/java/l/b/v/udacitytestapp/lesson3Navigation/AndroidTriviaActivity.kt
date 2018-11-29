fa0aa0754b9acea753ac5b7c8b4e54682a887776
Luis Barroso Vazquez <luis.barroso.vazquez@accenture.com>
2018-11-29 10:55:11 +0100

    Linked Navigation Controller to ActionBar.

--- app/src/main/java/l/b/v/udacitytestapp/lesson3Navigation/AndroidTriviaActivity.kt
+++ app/src/main/java/l/b/v/udacitytestapp/lesson3Navigation/AndroidTriviaActivity.kt
@@ -1,13 +1,32 @@
 package l.b.v.udacitytestapp.lesson3Navigation
 
+import android.app.Fragment
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import l.b.v.udacitytestapp.R
+import l.b.v.udacitytestapp.databinding.ActivityAndroidTriviaBinding
 
 class AndroidTriviaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_android_trivia)
+//        setContentView(R.layout.activity_android_trivia)
+
+        var binding = DataBindingUtil.setContentView<ActivityAndroidTriviaBinding>(this, R.layout.activity_android_trivia)
+
+        //setup nav controller
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
