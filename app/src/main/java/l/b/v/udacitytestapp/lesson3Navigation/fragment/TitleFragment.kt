5a289a06c9093f81f28c2c8ddad843392393c67a
Luis Barroso Vazquez <luis.barroso.vazquez@accenture.com>
2018-11-29 11:17:09 +0100

    Added an About screen that can be accessed from up right menu options in Title fragment.

--- app/src/main/java/l/b/v/udacitytestapp/lesson3Navigation/fragment/TitleFragment.kt
+++ app/src/main/java/l/b/v/udacitytestapp/lesson3Navigation/fragment/TitleFragment.kt
@@ -4,11 +4,11 @@ package l.b.v.udacitytestapp.lesson3Navigation.fragment
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import l.b.v.udacitytestapp.R
 import l.b.v.udacitytestapp.databinding.FragmentTitleBinding
 
@@ -26,8 +26,17 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener { view:View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
 
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController()) || super.onOptionsItemSelected(item)
+    }
 }
